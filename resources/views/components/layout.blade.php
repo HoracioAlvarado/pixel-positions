8cdc178fef0e0525e78590598000c8d5ee9e556b
Horacio Alvarado <halvarado@modernizacion.gob.ar>
2024-05-27 00:16:45 -0300

    Components tag, heading, job-card

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,26 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+<body class="bg-black text-white">
     
-    <div>
-        <nav>
+    <div class="px-10">
+        <nav class="flex justify-between items-center pt-4 border-b border-white/10">
             <div>
-                <a href="">
+                <a href="/">
                     <img src="{{ Vite::asset('resources/images/logo.svg')}}" alt="logo">
                 </a>
             </div>
-            <div>
-                links
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
             </div>
             <div>
-                post a job
+                <a href="#">Post a Job</a>
             </div>
         </nav>
 
-        <main>
+        <main class="mt-10 m-w-[986px] mx-auto">
             {{ $slot }}
         </main>
     </div>
